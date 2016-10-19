68b8e4b2a313b8e3efe1358510d4ea2f8e059d85
micamoon <micamoonzon@gmail.com>
2016-10-19 19:37:12 -0300

    Repaso

---
+++ Repaso/Repaso/Models/Stuff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Repaso.Models
+{
+    public class Stuff
+    {
+        public int StuffId { get; set; }
+        public string NameStuff { get; set; }
+        public int SizeStuff { get; set; }
+        public DateTime DateStuff { get; set; }
+    }
+
+}
